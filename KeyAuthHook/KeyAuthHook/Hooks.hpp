f747b997e7ad57bd6d47ec53d8f7faa98f329b97
NetworkMapper <176590097+NetworkMapper@users.noreply.github.com>
2024-08-07 14:22:14 +0300

    Update Hooks.hpp

--- KeyAuthHook/KeyAuthHook/Hooks.hpp
+++ KeyAuthHook/KeyAuthHook/Hooks.hpp
@@ -1,13 +1,15 @@
 #include <string>
+#include <iostream>
 #include <cstdio>
 #include <fstream>
 #include "Minhook/MinHook.h"
 #include "PatternScanner.hpp"
 
 namespace Patterns {
-        UINT64 Error = (UINT64)PTRSCAN::PatternScan(GetModuleHandle(NULL), "48 89 5C 24 10 57 48 81");
-	UINT64 Modify = (UINT64)PTRSCAN::PatternScan(GetModuleHandle(NULL), "48 89 5C 24 08 48 89 74 24 10 48");
-	UINT64 Req = (UINT64)PTRSCAN::PatternScan(GetModuleHandle(NULL), "48 89 5C 24 20 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C");
+	auto ModuleBase = GetModuleHandle(NULL);
+        auto Error = (UINT64)PTRSCAN::PatternScan(ModuleBase, "48 89 5C 24 10 57 48 81");
+	auto Modify = (UINT64)PTRSCAN::PatternScan(ModuleBase, "48 89 5C 24 08 48 89 74 24 10 48");
+	auto Req = (UINT64)PTRSCAN::PatternScan(ModuleBase, "48 89 5C 24 20 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C");
 }
 
 namespace Logger {
@@ -21,39 +23,41 @@ namespace Logger {
 }
 
 namespace Hooks {	
-	using ReqHooked = std::string(*)(std::string data, std::string url);
+	using ReqHooked = std::string(__cdecl*)(std::string data, std::string url);
 	ReqHooked ReqLook = nullptr;
 
-	inline void NothingHook() {
+	void NothingHook() {
 		// do nothing ...
 	}
 
-	inline std::string ReqHook(std::string data, std::string url) {
+	std::string ReqHook(std::string data, std::string url) {
 		if (data.find("type=log") != std::string::npos) {
 			// can change data example: type=log&pcuser=&message=&sessionid=&name=&ownerid=
 			auto Response = ReqLook(data, url);
 			Logger::Log(Response);
 			return Response;
 		}
-		if (data.find("type=webhook") != std::string::npos) {
+		else if (data.find("type=webhook") != std::string::npos) {
 			// can change data
 			auto Response = ReqLook(data, url);
 			Logger::Log(Response);
 			return Response;
 		}
-
-		if (data.find("type=login") != std::string::npos) {
+		else if (data.find("type=login") != std::string::npos) {
 			auto Response = ReqLook(data, url);
 			Logger::Log(Response);
 			// you can bypass keyauth here! just need to find the correct string can you do it?
 			return Response;
 		}
-		auto Response = ReqLook(data, url);
-		Logger::Log(Response);
-		return Response;
+		else {
+			auto Response = ReqLook(data, url);
+			return Response;
+		}
+
+		return "";
 	}
 
-	inline void Start() {
+	void Start() {
 		if (MH_Initialize() != MH_OK) {
 			printf("Error Initializing MinHook");
 		}
